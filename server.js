78fbdb58ec60bb25550e5bfb9b6e62cf76f7a855
dabergotzDesign <otakulp@gmx.de>
2023-03-09 10:59:31 +0100

    ejs navigation

--- server.js
+++ server.js
@@ -14,16 +14,34 @@ fs.readFile(
   'utf-8', 
   (error, data) => {
     const splitPattern = new RegExp("\r\n|\r|\n");
-    const products = data.split(splitPattern);
-    products.shift();
-    const pages = products
+    const products_data = data.split(splitPattern);
+    products_data.shift();
+    const products = products_data
       .filter(row => row.trim() !== '')
       .map(createHTML);
-     
-  
-    app.get('/product/:index', (req, res) => {
-      res.send("Product: " + pages[req.params.index]); 
+
+    app.set("view engine", "ejs");
+    
+    app.get("/", (req, res) => { 
+      res.render("index", {
+        products: products
+      }); 
     });
+
+    app.get('/product/:index', (req, res) => {
+      res.render("products",{
+        products: products,
+        index: req.params.index
+      }); 
+    });
+  
+    /* app.get('/product/:index', (req, res) => {
+      res.send("Product: " + products[req.params.index]); 
+    }); */
+
+    /* app.get('/', (req, res) => {
+      res.send("Product: " + products[req.params.index]); 
+    }); */
 
     app.listen(port, ip, () => {
         console.log(`Server running at http://${ip}:${port}/`);
@@ -35,12 +53,13 @@ fs.readFile(
 //Individual Product Pages
 const createHTML = row => {
   const fields = row.split('\t');
-  const page = `
-        <h1>${fields[0]}</h1>
-        <p>${fields[1]}</p>
-        <p>Installationsbefehl: <code>${fields[2]}</code></p>
-        <p><a href="${fields[3]}">${fields[3]}</a></p>`;
-  return page;
+  const product = {
+                    product_name: fields[0],
+                    product_desc: fields[1],
+                    product_install: fields[2],
+                    product_url: fields[3]
+                  };
+  return product;
 }
 
 
